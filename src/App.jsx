22389c54ae5dc15f0a7c6a28fc8b0735be8b2f5f
anjanadhikari7 <166963730+anjanadhikari7@users.noreply.github.com>
2024-09-26 17:53:31 +1000

    tailwind integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-5xl text-center font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
