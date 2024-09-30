c5a0d3c8b7241d49dc52072b7747112b47140e3b
anjanadhikari7 <166963730+anjanadhikari7@users.noreply.github.com>
2024-09-30 19:45:44 +1000

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,114 +1,193 @@
-// src/components/Contact.jsx
 import React, { useState } from "react";
-import { motion } from "framer-motion"; // Import motion for animations
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { CgMail } from "react-icons/cg";
+import { MdLocationOn } from "react-icons/md";
+import { motion } from "framer-motion";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
+  const [formDetails, setFormDetails] = useState({
+    firstName: "",
+    lastName: "",
     email: "",
+    phone: "",
     message: "",
   });
 
-  const [submitted, setSubmitted] = useState(false);
+  const [status, setStatus] = useState({
+    message: "",
+    success: false,
+  });
 
-  const handleChange = (e) => {
-    setFormData((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
+  const [buttonText, setButtonText] = useState("Send Message");
+
+  const onFormUpdate = (field, value) => {
+    setFormDetails({
+      ...formDetails,
+      [field]: value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission (e.g., send to an API or email)
-    console.log(formData);
-    setSubmitted(true);
-    setFormData({ name: "", email: "", message: "" });
+    setButtonText("Sending...");
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_APP_SERVICE_ID,
+        import.meta.env.VITE_APP_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_APP_PUBLIC_KEY
+      )
+      .then(
+        (response) => {
+          setButtonText("Send Message");
+          setStatus({
+            message: "Message sent successfully!",
+            success: true,
+          });
+          setFormDetails({
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setButtonText("Send Message");
+          setStatus({
+            message: "Failed to send message. Please try again.",
+            success: false,
+          });
+        }
+      );
   };
 
   return (
     <section
+      className="py-16 bg-gradient-to-b from-purple-500 to-indigo-600 text-white"
       id="contact"
-      className="py-20 bg-gradient-to-b from-purple-500 to-indigo-600 text-white"
     >
       <div className="container mx-auto text-center">
         <motion.h2
-          className="text-3xl font-bold mb-8"
+          className="text-4xl font-bold mb-8"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
         >
-          Get in Touch
+          Contact Me
         </motion.h2>
-        {submitted ? (
-          <motion.p
-            className="text-xl"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.3 }}
-          >
-            Thank you for your message! I'll get back to you soon.
-          </motion.p>
-        ) : (
-          <form className="max-w-xl mx-auto mt-10" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row justify-around items-start">
+          <div className="md:w-1/2 space-y-6 text-left p-4">
             <motion.div
-              className="mb-4"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.3 }}
+            >
+              <div className="flex items-center space-x-4 bg-white bg-opacity-10 p-4 rounded-md shadow-lg">
+                <BsFillTelephoneFill size={24} />
+                <span>0435662594</span>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+            >
+              <div className="flex items-center space-x-4 bg-white bg-opacity-10 p-4 rounded-md shadow-lg">
+                <CgMail size={24} />
+                <span>anjanadhikari179@gmail.com</span>
+              </div>
+            </motion.div>
+            <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.5 }}
             >
-              <input
-                type="text"
-                name="name"
-                placeholder="Your Name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-                className="w-full p-3 rounded bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-300 transition duration-300 ease-in-out"
-              />
+              <div className="flex items-center space-x-4 bg-white bg-opacity-10 p-4 rounded-md shadow-lg">
+                <MdLocationOn size={24} />
+                <span>Based in Sydney, Australia</span>
+              </div>
             </motion.div>
-            <motion.div
-              className="mb-4"
-              initial={{ opacity: 0, y: 20 }}
+          </div>
+          <div className="md:w-1/2 mt-12 md:mt-0 p-4">
+            <motion.form
+              onSubmit={handleSubmit}
+              className="space-y-6"
+              initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.6 }}
             >
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formDetails.firstName}
+                  placeholder="First Name"
+                  onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                  className="p-4 rounded-lg bg-white bg-opacity-10 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-300"
+                  required
+                />
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formDetails.lastName}
+                  placeholder="Last Name"
+                  onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                  className="p-4 rounded-lg bg-white bg-opacity-10 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-300"
+                  required
+                />
+              </div>
               <input
                 type="email"
                 name="email"
-                placeholder="Your Email"
-                value={formData.email}
-                onChange={handleChange}
+                value={formDetails.email}
+                placeholder="Email"
+                onChange={(e) => onFormUpdate("email", e.target.value)}
+                className="w-full p-4 rounded-lg bg-white bg-opacity-10 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-300"
                 required
-                className="w-full p-3 rounded bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-300 transition duration-300 ease-in-out"
               />
-            </motion.div>
-            <motion.div
-              className="mb-6"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8, delay: 0.7 }}
-            >
+              <input
+                type="tel"
+                name="phone"
+                value={formDetails.phone}
+                placeholder="Phone"
+                onChange={(e) => onFormUpdate("phone", e.target.value)}
+                className="w-full p-4 rounded-lg bg-white bg-opacity-10 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-300"
+                required
+              />
               <textarea
                 name="message"
+                rows={6}
+                value={formDetails.message}
                 placeholder="Your Message"
-                value={formData.message}
-                onChange={handleChange}
+                onChange={(e) => onFormUpdate("message", e.target.value)}
+                className="w-full p-4 rounded-lg bg-white bg-opacity-10 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-300"
                 required
-                rows="5"
-                className="w-full p-3 rounded bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-300 transition duration-300 ease-in-out"
-              ></textarea>
-            </motion.div>
-            <motion.button
-              type="submit"
-              className="bg-white text-purple-600 px-6 py-3 rounded-full font-semibold hover:bg-gray-100 transition"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              Send Message
-            </motion.button>
-          </form>
-        )}
+              />
+              <motion.button
+                type="submit"
+                className="w-full py-3 bg-indigo-500 rounded-lg text-white font-semibold hover:bg-indigo-400 transition duration-300"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                {buttonText}
+              </motion.button>
+              {status.message && (
+                <motion.div
+                  className={`mt-4 text-center ${
+                    status.success ? "text-green-500" : "text-red-500"
+                  }`}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                >
+                  {status.message}
+                </motion.div>
+              )}
+            </motion.form>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { FaHome, FaUser, FaEnvelope, FaProjectDiagram } from "react-icons/fa";
+import { GiGiftOfKnowledge } from "react-icons/gi";
 import { motion } from "framer-motion";
+
+const navItems = [
+  { icon: <FaHome />, name: "Home", to: "home" },
+  { icon: <FaUser />, name: "About", to: "about" },
+  { icon: <FaProjectDiagram />, name: "Projects", to: "projects" },
+  { icon: <GiGiftOfKnowledge />, name: "Skills", to: "skills" },
+  { icon: <FaEnvelope />, name: "Contact", to: "contact" },
+];
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolling, setScrolling] = useState(false);
+  const [headerHeight, setHeaderHeight] = useState("10vh");
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrolling(window.scrollY > 50);
+      if (window.scrollY > 40) {
+        setHeaderHeight("6vh");
+        setScrolling(true);
+      } else {
+        setHeaderHeight("10vh");
+        setScrolling(false);
+      }
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -23,98 +40,97 @@ const Navbar = () => {
   };
 
   return (
-    <motion.nav
-      className={`fixed w-full z-20 transition duration-300 ${
-        scrolling
-          ? "bg-purple-700 shadow-md"
-          : "bg-gradient-to-r from-purple-500 to-pink-500"
-      }`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-    >
-      <div className="container mx-auto flex justify-between items-center p-4">
-        <div className="text-white text-2xl font-bold flex items-center">
-          <img
-            src="/logo.png" // Placeholder for your logo
-            alt="logo"
-            className="w-8 h-8 mr-2"
-          />
-          MyPortfolio
-        </div>
+    <nav className="z-50 bg-customdark fixed w-full">
+      <motion.div
+        initial={{ y: "-100%" }}
+        animate={{ y: 0 }}
+        transition={{ type: "tween", duration: 0.6, ease: "easeInOut" }}
+        className={`fixed top-0 left-0 w-full z-50 transition duration-300 ${
+          scrolling
+            ? "bg-purple-700 shadow-md"
+            : "bg-gradient-to-r from-purple-500 to-pink-500"
+        }`}
+      >
+        <div
+          className="flex justify-between items-center p-4 mx-auto max-w-[1440px]"
+          style={{ height: headerHeight, transition: "height 0.3s ease" }}
+        >
+          <div className="text-white text-2xl font-bold flex items-center">
+            <img
+              src="/logo.png" // Placeholder for your logo
+              alt="logo"
+              className="w-8 h-8 mr-2"
+            />
+            MyPortfolio
+          </div>
 
-        <ul className="hidden md:flex space-x-8 text-white text-lg">
-          <li
-            className="hover:text-gray-200 transition cursor-pointer"
-            onClick={() => handleScrollTo("home")}
-          >
-            Home
-          </li>
-          <li
-            className="hover:text-gray-200 transition cursor-pointer"
-            onClick={() => handleScrollTo("about")}
-          >
-            About
-          </li>
-          <li
-            className="hover:text-gray-200 transition cursor-pointer"
-            onClick={() => handleScrollTo("projects")}
-          >
-            Projects
-          </li>
-          <li
-            className="hover:text-gray-200 transition cursor-pointer"
-            onClick={() => handleScrollTo("contact")}
-          >
-            Contact
-          </li>
-        </ul>
+          <div className="hidden md:flex md:justify-between md:space-x-8 text-white">
+            {navItems.map((item, index) => (
+              <div
+                key={index}
+                className="group flex items-center cursor-pointer hover:text-yellow-500 transition"
+                onClick={() => handleScrollTo(item.to)}
+              >
+                <span className="flex flex-col items-center">
+                  {item.icon}
+                  <span>{item.name}</span>
+                </span>
+              </div>
+            ))}
+          </div>
 
-        <div className="md:hidden">
-          <button
+          <div
+            className="md:hidden text-white"
             onClick={() => setIsOpen(!isOpen)}
-            className="text-white focus:outline-none"
           >
             {isOpen ? (
-              <XMarkIcon className="h-8 w-8" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                aria-hidden="true"
+                className="w-[2rem] h-[2rem]"
+              >
+                <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+              </svg>
             ) : (
-              <Bars3Icon className="h-8 w-8" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                aria-hidden="true"
+                className="w-[2rem] h-[2rem]"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2 3.75A.75.75 0 0 1 2.75 3h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 3.75ZM2 8a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 8Zm0 4.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
             )}
-          </button>
+          </div>
         </div>
-      </div>
+      </motion.div>
 
       {isOpen && (
-        <div className="md:hidden bg-purple-700 text-white">
-          <ul className="flex flex-col items-center space-y-6 py-4">
-            <li
-              className="hover:text-gray-200 transition"
-              onClick={() => handleScrollTo("home")}
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: 0 }}
+          transition={{ type: "tween", duration: 0.4, ease: "easeInOut" }}
+          className="fixed top-0 right-0 h-screen w-full bg-purple-700 text-white flex flex-col items-center space-y-6 py-16"
+        >
+          {navItems.map((item, index) => (
+            <div
+              key={index}
+              className="group flex items-center text-2xl font-semibold cursor-pointer hover:text-yellow-500 transition"
+              onClick={() => handleScrollTo(item.to)}
             >
-              Home
-            </li>
-            <li
-              className="hover:text-gray-200 transition"
-              onClick={() => handleScrollTo("about")}
-            >
-              About
-            </li>
-            <li
-              className="hover:text-gray-200 transition"
-              onClick={() => handleScrollTo("projects")}
-            >
-              Projects
-            </li>
-            <li
-              className="hover:text-gray-200 transition"
-              onClick={() => handleScrollTo("contact")}
-            >
-              Contact
-            </li>
-          </ul>
-        </div>
+              {item.name}
+            </div>
+          ))}
+        </motion.div>
       )}
-    </motion.nav>
+    </nav>
   );
 };
 
